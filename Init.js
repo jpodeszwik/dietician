90e054a963ae87a28d0eb111da5ef578fd8ba658
damienix <damienix1@gmail.com>
2015-09-05 16:32:56 +0200

    Move initializer to init.js

---
+++ Init.js
@@ -0,0 +1,31 @@
+$(function () {
+    var dietId = url('?id');
+    if (dietId != null) {
+        $.get('http://zbiki.ddns.net/diets/diet/' + dietId + '/_source', function onSuccess(data) {
+            var mealList = data['mealList'];
+            var mealListModel = new MealList();
+
+            //somehow MealList model didn't want to load the whole json
+            mealList.forEach(function (meal) {
+                var mealModel = new Meal({
+                    name: meal['name'],
+                    productList: new ProductList(meal['productList'])
+                });
+                mealListModel.add(mealModel);
+            });
+
+            var mealListView = new MealListView(mealListModel);
+        });
+    } else {
+        var mealListView = new MealListView(new MealList());
+    }
+
+    $.get('http://zbiki.ddns.net/products/_search?size=1000', function onSuccess(data) {
+        var hits = data["hits"]["hits"];
+
+        hits.forEach(function (hit) {
+            var product = hit["_source"];
+            products.addProduct(product);
+        });
+    });
+});
--- ProductView.js
+++ ProductView.js
@@ -166,37 +166,3 @@ var ProductListView = Backbone.View.extend({
         return this.model.summaryValue(param_name).toFixed(2);
     }
 });
-
-$(function () {
-
-
-    var dietId = url('?id');
-    if (dietId != null) {
-        $.get('http://zbiki.ddns.net/diets/diet/' + dietId + '/_source', function onSuccess(data) {
-            var mealList = data['mealList'];
-            var mealListModel = new MealList();
-
-            //somehow MealList model didn't want to load the whole json
-            mealList.forEach(function (meal) {
-                var mealModel = new Meal({
-                    name: meal['name'],
-                    productList: new ProductList(meal['productList'])
-                });
-                mealListModel.add(mealModel);
-            });
-
-            var mealListView = new MealListView(mealListModel);
-        });
-    } else {
-        var mealListView = new MealListView(new MealList());
-    }
-
-    $.get('http://zbiki.ddns.net/products/_search?size=1000', function onSuccess(data) {
-        var hits = data["hits"]["hits"];
-
-        hits.forEach(function (hit) {
-            var product = hit["_source"];
-            products.addProduct(product);
-        });
-    });
-});
