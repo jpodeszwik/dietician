b20805a15dbef70b454ce2fb5e8fc2213679800e
damienix <damienix1@gmail.com>
2015-09-06 03:10:14 +0200

    Refactor

--- Init.js
+++ Init.js
@@ -1,22 +1,27 @@
 $(function () {
-    nunjucks.configure("", {web: {useCache: true}});
+    configureLibs();
+    setupApp();
+});
 
+function configureLibs() {
+    nunjucks.configure("", {web: {useCache: true}});
     $.fn.editable.defaults.mode = 'inline';
     $.fn.editable.defaults.showbuttons = false;
     $.fn.editable.defaults.showbuttons = false;
     $.fn.editable.defaults.onblur = 'submit';
-
     Backbone.Marionette.Renderer.render = function (template, data) {
         if (_.isFunction(template)) {
             return template(data);
         }
         return nunjucks.render(template, data);
     };
+}
 
+var setupApp = function () {
     var dietId = url('?id');
 
+    var days = new DayCollection();
     var mealListModel = new MealList();
-    var mealListView = new MealListView(mealListModel);
 
     if (dietId != null) {
         Search.Load(dietId, mealListModel);
@@ -24,19 +29,17 @@ $(function () {
         var firstMeal = new Meal();
         firstMeal.set('name', "Meal 1");
         mealListModel.add(firstMeal);
+
+        days.add(new Day({name: "Monday", meals: mealListModel, active: true}));
+        days.add(new Day({name: "Tuesday", meals: new MealList()}));
+        days.add(new Day({name: "Wednesday", meals: new MealList()}));
+        days.add(new Day({name: "Thursday", meals: new MealList()}));
+        days.add(new Day({name: "Friday", meals: new MealList()}));
     }
 
     var caloricIntakeView = new CaloricIntakeView();
-
-
-    var days = new DayCollection();
-    days.add(new Day({name: "Monday", meals: mealListModel  , active: true}));
-    days.add(new Day({name: "Tuesday", meals: new MealList()}));
-    days.add(new Day({name: "Wednesday", meals: new MealList()}));
-    days.add(new Day({name: "Thursday", meals: new MealList()}));
-    days.add(new Day({name: "Friday", meals: new MealList()}));
+    var mealListView = new MealListView(mealListModel);
     var dayCollectionView = new DayCollectionView({collection: days});
     dayCollectionView.setElement($('.days-list'));
     dayCollectionView.render();
-
-});
+};
