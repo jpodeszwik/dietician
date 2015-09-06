0958b0075321be143a2cba6c930e9cf7a8bf3d86
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2015-09-06 04:31:10 +0200

    Added calories calculation

--- caloric-intake/CaloricIntakeModel.js
+++ caloric-intake/CaloricIntakeModel.js
@@ -15,12 +15,78 @@ var CaloricIntakeModel = Backbone.Model.extend({
         aerobicTrainingIntensity: 'Medium'
     },
 
+    int: function (name) {
+        return parseInt(this.get(name));
+    },
+
     bmr: function () {
-        var basicBmr = (9.99 * this.get('weight')) + (6.25 * this.get('height')) - (4.92 * this.get('age'));
+        var basicBmr = (9.99 * this.int('weight')) + (6.25 * this.int('height')) - (4.92 * this.int('age'));
         if (this.get('sex') === 'Male') {
             return basicBmr + 5;
         } else {
             return basicBmr - 161;
         }
+    },
+
+    weightTrainingKcal: function () {
+        var kcalPerMinMap = {
+            'Small': 7,
+            'Medium': 8,
+            'High': 9
+        };
+        var kcalPerMin = kcalPerMinMap[this.get('weightTrainingIntensity')];
+
+        var epocMap = {
+            'Endomorph': 0.04,
+            'Mesomorph': 0.055,
+            'Ectomorph': 0.07
+        };
+        var epoc = epocMap[this.get('bodyType')];
+
+        var timesPerWeek = this.int('weightTrainingTimesPerWeek');
+
+        return (timesPerWeek * this.int('weightTrainingAverageTime') * kcalPerMin) +
+            (timesPerWeek * epoc * this.bmr());
+    },
+
+    aerobicTrainingKcal: function () {
+        var kcalPerMinMap = {
+            'Small': 5,
+            'Medium': 7.5,
+            'High': 10
+        };
+        var kcalPerMin = kcalPerMinMap[this.get('aerobicTrainingIntensity')];
+
+        var epocMap = {
+            'Small': 5,
+            'Medium': 35,
+            'High': 180
+        };
+        var epoc = epocMap[this.get('aerobicTrainingIntensity')];
+
+        var timesPerWeek = this.int('aerobicTrainingTimesPerWeek');
+        return (timesPerWeek * this.int('aerobicTrainingAverageTime') * kcalPerMin) +
+            (timesPerWeek * epoc);
+    },
+
+    tea: function () {
+        return (this.weightTrainingKcal() + this.aerobicTrainingKcal()) / 7;
+    },
+
+    neat: function () {
+        var neatMap = {
+            'Endomorph': 300,
+            'Mesomorph': 450,
+            'Ectomorph': 800
+        };
+        return neatMap[this.get('bodyType')];
+    },
+
+    tef: function () {
+        return 0.08 * (this.bmr() + this.tea() + this.neat());
+    },
+
+    tdee: function () {
+        return this.bmr() + this.tea() + this.neat() + this.tef();
     }
 });
