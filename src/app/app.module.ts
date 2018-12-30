dd398513796e07f3dc5ddcd6690f30f994bf54c6
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-12-30 16:31:01 +0100

    WIP: ingredient livesearch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatTableModule, MatInputModule } from '@angular/material';
+import { MatToolbarModule, MatTableModule, MatInputModule, MatAutocompleteModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 import { MealComponent } from './meal/meal.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IngredientInputComponent } from './meal/ingredient-input/ingredient-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MealComponent,
+    IngredientInputComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,9 @@ import { FormsModule } from '@angular/forms';
     AngularFirestoreModule,
     MatToolbarModule,
     FormsModule,
+    ReactiveFormsModule,
     MatInputModule,
+    MatAutocompleteModule,
     MatTableModule,
     FlexLayoutModule,
   ],
---
+++ src/app/meal/ingredient-input/ingredient-input.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IngredientInputComponent } from './ingredient-input.component';
+
+describe('IngredientInputComponent', () => {
+  let component: IngredientInputComponent;
+  let fixture: ComponentFixture<IngredientInputComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ IngredientInputComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IngredientInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
