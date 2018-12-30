c8311b5652be50adfc786a430c736cdd0335b3ef
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-12-30 20:32:15 +0100

    Added row with summary values to meals

--- src/app/meal/meal.ts
+++ src/app/meal/meal.ts
@@ -8,4 +8,28 @@ export class Meal {
     this.name = name;
     this.ingredients = ingredients;
   }
+
+  totalProteins(): number {
+    return this.sumBy(i => i.proteins());
+  }
+
+  totalCarbohydrates(): number {
+    return this.sumBy(i => i.carbohydrates());
+  }
+
+  totalFats(): number {
+    return this.sumBy(i => i.fats());
+  }
+
+  totalNutritionValue(): number {
+    return this.sumBy(i => i.nutritionValue());
+  }
+
+  totalWeight(): number {
+    return this.sumBy(i => i.weight);
+  }
+
+  private sumBy(fn: (MealIngredient) => number): number {
+    return this.ingredients.map(i => fn(i)).reduce((prev, current) => current + prev, 0);
+  }
 }
